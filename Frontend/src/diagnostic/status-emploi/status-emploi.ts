993fd76129d7740a134c1d9e71453cbb33b94a78
Wahchaka21 <quentyn.petitjean@hotmail.com>
2026-03-26 16:59:36 +0100

    ajout

--- Frontend/src/diagnostic/status-emploi/status-emploi.ts
+++ Frontend/src/diagnostic/status-emploi/status-emploi.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-status-emploi',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './status-emploi.html',
   styleUrl: './status-emploi.css',
 })
-export class StatusEmploi {}
+export class StatusEmploi {
+  interimaire: boolean = false
+  interimaireTexte: string | null = null
+  cdd: boolean = false
+  cddTexte: string | null = null
+  cdi: boolean = false
+  cdiTexte: string | null = null
+  apprentissage: boolean = false
+  apprentissageTexte: string | null = null
+  contratAide: boolean = false
+  contratAideTexte: string | null = null
+  serviceCivique: boolean = false
+  serviceCiviqueTexte: string | null = null
+}
--- Frontend/src/diagnostic/status-mobilite/status-mobilite.ts
+++ Frontend/src/diagnostic/status-mobilite/status-mobilite.ts
@@ -1,9 +1,23 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-status-mobilite',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './status-mobilite.html',
   styleUrl: './status-mobilite.css',
 })
-export class StatusMobilite {}
+export class StatusMobilite {
+  codeObtenu: boolean = false
+  codeObtenuTexte: string | null = null
+  finParcoursConduite: boolean = false
+  finParcoursConduiteTexte: string | null = null
+  permisEtrangerNonValide: boolean = false
+  permisEtrangerNonvalideTexte: string | null = null
+  codeEnCours: boolean = false
+  codeEnCoursTexte: string | null = null
+  permisB: boolean = false
+  permisBTexte: string | null = null
+  permisAutres: boolean = false
+  permisAutresTexte: string | null = null
+}
