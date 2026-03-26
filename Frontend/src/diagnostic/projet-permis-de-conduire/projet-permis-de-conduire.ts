5a11f0f23f145eae754478b884d907a6021a2d54
Wahchaka21 <quentyn.petitjean@hotmail.com>
2026-03-26 16:02:34 +0100

    complémentation style du doc

--- Frontend/src/diagnostic/projet-permis-de-conduire/projet-permis-de-conduire.ts
+++ Frontend/src/diagnostic/projet-permis-de-conduire/projet-permis-de-conduire.ts
@@ -1,9 +1,15 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-projet-permis-de-conduire',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './projet-permis-de-conduire.html',
   styleUrl: './projet-permis-de-conduire.css',
 })
-export class ProjetPermisDeConduire {}
+export class ProjetPermisDeConduire {
+  estimiationPermis: boolean = false
+  estimationText: string | null = null
+  inscritAE: boolean = false
+  inscritAETexte: string | null = null
+}
