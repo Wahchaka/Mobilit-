bd0aeb6205029ebdfb97e2dee49cf6432b8bd7b2
Wahchaka21 <quentyn.petitjean@hotmail.com>
2026-03-26 16:18:50 +0100

    style de la nav bar

--- Frontend/src/nav-bar/nav-bar.ts
+++ Frontend/src/nav-bar/nav-bar.ts
@@ -1,8 +1,11 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
 
 @Component({
   selector: 'app-nav-bar',
-  imports: [],
+  imports: [RouterModule, CommonModule, FormsModule],
   templateUrl: './nav-bar.html',
   styleUrl: './nav-bar.css',
 })
