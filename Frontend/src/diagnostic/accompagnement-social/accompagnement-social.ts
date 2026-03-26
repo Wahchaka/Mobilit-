fd515c1e7a8c4350f1d0175d91b80ad313221fdc
Wahchaka21 <quentyn.petitjean@hotmail.com>
2026-03-26 14:49:25 +0100

    connection bdd valide

--- Frontend/src/diagnostic/accompagnement-social/accompagnement-social.ts
+++ Frontend/src/diagnostic/accompagnement-social/accompagnement-social.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-accompagnement-social',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './accompagnement-social.html',
   styleUrl: './accompagnement-social.css',
 })
-export class AccompagnementSocial {}
+export class AccompagnementSocial {
+  Autre: boolean = false
+  quoi: string | null = null
+}
--- Frontend/src/diagnostic/situation-familiale/situation-familiale.ts
+++ Frontend/src/diagnostic/situation-familiale/situation-familiale.ts
@@ -1,9 +1,14 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-situation-familiale',
-  imports: [],
+  imports: [CommonModule, FormsModule],
   templateUrl: './situation-familiale.html',
   styleUrl: './situation-familiale.css',
 })
-export class SituationFamiliale {}
+export class SituationFamiliale {
+  aDesEnfants: boolean = false
+  nombreEnfants: number | null = null
+}
