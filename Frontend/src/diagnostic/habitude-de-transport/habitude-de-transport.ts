8f54d5e6891cd8f3057d61d90641cbd4db724a7b
Wahchaka21 <quentyn.petitjean@hotmail.com>
2026-03-31 12:23:04 +0200

    prob reglé, manque plus que 'autre' dans motif

--- Frontend/src/diagnostic/habitude-de-transport/habitude-de-transport.ts
+++ Frontend/src/diagnostic/habitude-de-transport/habitude-de-transport.ts
@@ -1,50 +1,55 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-habitude-de-transport',
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   templateUrl: './habitude-de-transport.html',
   styleUrl: './habitude-de-transport.css',
 })
 export class HabitudeDeTransport {
-  habitudes = [1, 2, 3]
+  habitudes = [
+    { autre: false, autreValue: "" },
+    { autre: false, autreValue: "" },
+    { autre: false, autreValue: "" }
+  ]
 
-transports = [
-  'Voiture',
-  '2 roues motorisés',
-  'Bus',
-  'Train',
-  'Vélo/trottinette',
-  'Marches',
-  'Autres'
-]
+  transports = [
+    'Voiture',
+    '2 roues motorisés',
+    'Bus',
+    'Train',
+    'Vélo/trottinette',
+    'Marches',
+    'Autres'
+  ]
 
-frequences = [
-  'Tous les jours ou presque',
-  '2/3 jours par semaine',
-  '1 jour/semaine',
-  '2/3 jours par mois',
-  '1 jour/mois',
-  'Moins'
-]
+  frequences = [
+    'Tous les jours ou presque',
+    '2/3 jours par semaine',
+    '1 jour/semaine',
+    '2/3 jours par mois',
+    '1 jour/mois',
+    'Moins'
+  ]
 
-perimetres = [
-  'Domicile',
-  'Quartier',
-  'Ville/commune',
-  'Agglomération',
-  'Aire d’attraction',
-  'Département',
-  'Région',
-  'France',
-  'Plus'
-]
+  perimetres = [
+    'Domicile',
+    'Quartier',
+    'Ville/commune',
+    'Agglomération',
+    'Aire d’attraction',
+    'Département',
+    'Région',
+    'France',
+    'Plus'
+  ]
 
-motifs = [
-  'Vie professionnelle',
-  'Vie quotidienne',
-  'Vie sociale et loisirs',
-  'Autres'
-]
+  motifs = [
+    'Vie professionnelle',
+    'Vie quotidienne',
+    'Vie sociale et loisirs',
+    'Autres'
+  ]
 }
